03ccc2423cf7a32329ac4ad3f1c563dfab6be42a
xiaowu <xiaohuiwu521@gmail.com>
2017-04-19 22:08:04 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
           post :publish
           post :hide
         end
+        resources :resumes 
         end
     end
 
@@ -16,6 +17,6 @@ Rails.application.routes.draw do
 
   resources :jobs do
     resources :resumes
-  end 
+  end
   root 'jobs#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

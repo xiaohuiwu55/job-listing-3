22e4f0d027726317e526f4bf16b1da310703abba
xiaowu <xiaohuiwu521@gmail.com>
2017-04-24 00:21:04 +0800

    add is_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   def require_is_admin
@@ -7,4 +8,8 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+  protected
+    def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:is_admin])
+    end
 end

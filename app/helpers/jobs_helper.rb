4f83f3f75a68a09bcc365f4385a0fa4d0e71cfe5
xiaowu <xiaohuiwu521@gmail.com>
2017-04-23 23:59:27 +0800

    backstep8

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -7,12 +7,5 @@ module JobsHelper
     end
   end
 
-  def render_highlight_content(job,query_string)
-    excerpt_cont = excerpt(job.title, query_string, radius: 500)
-    highlight(excerpt_cont, query_string)
-  end
-  def render_highlight_content(job,query_string)
-    excerpt_cont = excerpt(job.title, query_string, radius: 500)
-    highlight(excerpt_cont, query_string)
-  end
+  
 end
